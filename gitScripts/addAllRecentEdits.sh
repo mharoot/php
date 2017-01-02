475b5b0f108d39288f84705a51a548b6e3f8bdba
michaelh818 <michaelharootoonyan@gmail.com>
2017-01-01 17:09:22 -0800

    "update enables pushing to branch of choice"

--- gitScripts/addAllRecentEdits.sh
+++ gitScripts/addAllRecentEdits.sh
@@ -16,21 +16,27 @@ function addGit() {
     git add .
     case $(isEmpty "$1") in
     "0") 
-        git commit -m "message n/a";; # commit without message
+        git commit -m "message n/a";; # commit without message gives default message
     "1") 
         git commit -m "$1" ;; #commit with message
     *) #default case
-        echo "error with commit" ;; 
+        echo "error with commit message" ;; 
     esac
-    git push origin master
+
+    if [ isEmpty "$2" == "0" ] ; then 
+	git push origin master # empty by default push to origin master
+    else
+	git push origin "$2"   # push to branch
+    fi
 }
 
 
 
 function main() {
    echo "Enter a message if you'd like with your git commit"
-   read message 
-   addGit "$message" #important to wrap in string because if you type testing addAllRecentEdits.sh script it will drop everything after testing
+   read commitMessage 
+   echo "Enter a branch name if you want to load to a different branch other than master"
+   addGit "$commitMessage" "$branch" #important to wrap in string because if you type testing addAllRecentEdits.sh script it will drop everything after testing
 }
 
 #run main
