86bd0f81e3688288c4a1fceea540f76cbad0ef5b
michaelh818 <michaelharootoonyan@gmail.com>
2017-01-03 19:01:40 -0800

    "updated README to explain gitScripts"

--- gitScripts/getEntireRepo.sh
+++
@@ -1,2 +0,0 @@
-#grabs all files overwrites any changes made by others including to your own
-git pull origin master
--- gitScripts/resetFileToStageDirectory.sh
+++
@@ -1,2 +0,0 @@
-#run script with one argument provided = filename
-git checkout HEAD $1
