7ce675163788a6a15d20a574bf98728ff765594c
mharoot <michaelharootoonyan@yahoo.com>
2017-08-07 06:26:08 -0700

    fixed phone number dash placement and error checking

--- website/assets/js/phoneNumberFormValidator.js
+++ website/assets/js/phoneNumberFormValidator.js
@@ -1,29 +1,56 @@
 var inputCounter  = 0;
 var phoneNumberInput = document.getElementById('phoneNumber');
+phoneNumberInput.setAttribute( "autocomplete", "off" );
+phoneNumberInput.setAttribute( "autofill", "off" );
+
 phoneNumberInput.onkeydown = myFunction;
+phoneNumberInput.oninput = isLetter;
+
+function isLetter() {
+  var checkPosition = phoneNumberInput.value.length - 1;
+  var valueToCheck  = phoneNumberInput.value.substring(checkPosition, phoneNumberInput.value.length);
+
+
+  if((isNaN(valueToCheck) && valueToCheck != '-') || checkPosition > 11)
+    phoneNumberInput.value = '';
+  else if(valueToCheck === '-' && (checkPosition === 3 || checkPosition === 7) )
+  {
+    return;
+  }
+  else if (isNaN(valueToCheck) )
+  {
+    phoneNumberInput.value = '';
+  }
+}
 
 function myFunction() {
-	var key = event.keyCode || event.charCode;
-    var isNumber = (key > 47 && key < 58) || (key > 95 && key < 106);
+
+	  var key = event.keyCode || event.charCode;
+    var isNumber = (47 < key && key < 58) || (95 < key && key < 106);
     var isDelete = key == 8 || key == 46; //backspace or delete
-    
-	if (isNumber) inputCounter++;
-    else if (isDelete) {
-    	inputCounter--;
+    var isDirectional = 36 < key && key < 41 ;
+    var isDash = 109 == key || 189 == key;
+    var checkPosition = phoneNumberInput.value.length;
+    var valueToCheck  = phoneNumberInput.value.substring(checkPosition, phoneNumberInput.value.length+1);
+
+	  if (isNumber) inputCounter++;
+    else if (isDash && (inputCounter == 3 || inputCounter == 7) ) {
+      inputCounter++;
+      return;
+    }
+    else {
+      phoneNumberInput.value = '';
+    	inputCounter = 0;
     	return;
     } 
-    else myFunctionUtil();
     
-    if (inputCounter == 4 || inputCounter == 7) {
+    if (inputCounter == 4 || inputCounter == 8) {
       var dashAppend = phoneNumberInput.value+'-';
       phoneNumberInput.value = dashAppend;
+      inputCounter++;
     }
-    if ( phoneNumberInput.value.length == 12) myFunctionUtil();
 
-
-
-
-    
+    if ( phoneNumberInput.value.length == 12) myFunctionUtil(); 
 }
 
 function myFunctionUtil() {
